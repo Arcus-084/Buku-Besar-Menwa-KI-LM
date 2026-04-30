3e8d7c8760cf4a131e2b7d8f3523d729719e2253
Arcus-084 <guesia3@gmail.com>
2026-05-01 00:54:26 +0700

    Refactor account.py to improve structure and clarity

--- account.py
+++ account.py
@@ -10,41 +10,6 @@ supabase: Client = create_client(url, key)
 # --- KONFIGURASI HALAMAN ---
 st.set_page_config(page_title="Buku Besar MENWA KI LM", layout="centered")
 
-st.markdown(
-    """
-    <style>
-    /* 1. Sembunyikan elemen standar */
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden !important;}
-    header {visibility: hidden !important;}
-    
-    /* 2. Sembunyikan Toolbar Pojok Kanan Bawah */
-    [data-testid="stStatusWidget"] {
-        display: none !important;
-    }
-
-    /* 3. TRIK UTAMA: Menimpa Footer Streamlit */
-    /* Kita buat kotak penutup di bagian paling bawah layar */
-    .stApp::after {
-        content: "";
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        height: 50px; /* Sesuaikan tinggi untuk menutup bar sponsor */
-        background-color: #0E1117; /* Ganti dengan warna background aplikasi kamu */
-        z-index: 999999;
-    }
-
-    /* 4. Menyesuaikan layout agar konten tidak tertutup kotak penutup */
-    .stApp {
-        margin-bottom: 50px !important;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 # Inisialisasi status aplikasi (Splash Screen & Navigasi)
 if 'auth' not in st.session_state:
     st.session_state.auth = False
